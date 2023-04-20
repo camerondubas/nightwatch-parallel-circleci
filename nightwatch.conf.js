5472aeae64bfe7e21b994f1d8c02d1e4b5e8ffcb
Cameron Dubas <camerondubas@gmail.com>
2023-04-20 15:57:38 +0100

    fix: start maximized

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -15,20 +15,10 @@ module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
   src_folders: ["test", "nightwatch"],
-
-  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
   page_objects_path: [],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
   custom_commands_path: [],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
   custom_assertions_path: [],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
-
-  // See https://nightwatchjs.org/guide/concepts/test-globals.html
   globals_path: "",
 
   webdriver: {},
@@ -50,23 +40,13 @@ module.exports = {
 
       desiredCapabilities: {
         browserName: "chrome",
-      },
-
-      webdriver: {
-        start_process: true,
-        server_path: "",
-      },
-    },
-
-    chrome: {
-      desiredCapabilities: {
-        browserName: "chrome",
         "goog:chromeOptions": {
           // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
           //
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
           w3c: true,
           args: [
+            "start-maximized",
             //'--no-sandbox',
             //'--ignore-certificate-errors',
             //'--allow-insecure-localhost',
@@ -78,9 +58,6 @@ module.exports = {
       webdriver: {
         start_process: true,
         server_path: "",
-        cli_args: [
-          // --verbose
-        ],
       },
     },
   },
