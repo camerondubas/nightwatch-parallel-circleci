bba857b39c8279e3dc066bfc44eb9e987dc7f43e
Cameron Dubas <camerondubas@gmail.com>
2023-04-25 12:22:14 +0100

    restore conf

---
+++ nightwatch.conf.js
@@ -0,0 +1,70 @@
+// Refer to the online docs for more details:
+// https://nightwatchjs.org/gettingstarted/configuration/
+//
+
+//  _   _  _         _      _                     _          _
+// | \ | |(_)       | |    | |                   | |        | |
+// |  \| | _   __ _ | |__  | |_ __      __  __ _ | |_   ___ | |__
+// | . ` || | / _` || '_ \ | __|\ \ /\ / / / _` || __| / __|| '_ \
+// | |\  || || (_| || | | || |_  \ V  V / | (_| || |_ | (__ | | | |
+// \_| \_/|_| \__, ||_| |_| \__|  \_/\_/   \__,_| \__| \___||_| |_|
+//             __/ |
+//            |___/
+
+module.exports = {
+  // An array of folders (excluding subfolders) where your tests are located;
+  // if this is not specified, the test source must be passed as the second argument to the test runner.
+  src_folders: ["dist"],
+  page_objects_path: [],
+  custom_commands_path: [],
+  custom_assertions_path: [],
+  plugins: [],
+  globals_path: "",
+
+  webdriver: {},
+
+  test_workers: {
+    enabled: false,
+  },
+
+  test_settings: {
+    default: {
+      disable_error_log: false,
+      launch_url: "http://localhost:3000",
+
+      screenshots: {
+        enabled: false,
+        path: "screens",
+        on_failure: true,
+      },
+
+      desiredCapabilities: {
+        browserName: "chrome",
+        "goog:chromeOptions": {
+          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
+          //
+          // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
+          w3c: true,
+          args: [
+            "start-maximized",
+            //'--no-sandbox',
+            //'--ignore-certificate-errors',
+            //'--allow-insecure-localhost',
+            //'--headless'
+          ],
+        },
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: "",
+      },
+    },
+  },
+
+  usage_analytics: {
+    enabled: true,
+    log_path: "./logs/analytics",
+    client_id: "190338ac-de34-4761-8ed5-066872399ac1",
+  },
+};
